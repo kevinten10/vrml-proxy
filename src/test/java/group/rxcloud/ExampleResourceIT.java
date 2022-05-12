a3f01418f7b862ded7ad20f00b6c5cd7b7a40dd5
kevinten <596823919@qq.com>
2022-05-12 20:27:22 +0800

    Initial commit

---
+++ src/test/java/group/rxcloud/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package group.rxcloud;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
